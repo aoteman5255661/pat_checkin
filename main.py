bc7167c9d54a78d896b197817b2afe556d75fc9b
Your Name <394156907@qq.com>
2022-03-02 19:53:24 +0800

    1

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ response = requests.request("POST", url, headers=headers).json()
 
 print(response)
 print(response['error']['message'])
+
+token = os.getenv('pushplus_token')
+if not token:
+  print("没有设置pushplus_token返回")
+url = "http://www.pushplus.plus/send?token={}&content={}".format(token, response)
+response1 = requests.request("POST", url)
+print(response1.text)
