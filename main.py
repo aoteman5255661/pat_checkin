326140c5f8ed2b26a0c60e8453e2c6256ef0f531
Your Name <394156907@qq.com>
2022-03-02 19:55:49 +0800

    1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 import os
 url = "https://pintia.cn/api/users/checkin"
 
@@ -15,6 +16,7 @@ print(response['error']['message'])
 token = os.getenv('pushplus_token')
 if not token:
   print("没有设置pushplus_token返回")
+  sys.exit()
 url = "http://www.pushplus.plus/send?token={}&content={}".format(token, response)
 response1 = requests.request("POST", url)
 print(response1.text)
